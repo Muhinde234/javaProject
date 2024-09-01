8a00db943fd11659964db2c09cc5dbc17813e757
Muhinde <igirimpuhwedosta@gmail.com>
2024-09-01 15:51:44 +0200

    journey learning java

---
+++ stringConc.java
@@ -0,0 +1,8 @@
+public class stringConc {
+    public static void main(String[] args) {
+        String firstname="Igirimpuhwe";
+        String lastname="Dosta";
+        System.out.println(firstname .  concat(lastname));
+    }
+    
+}
---
+++ stringLength.java
@@ -0,0 +1,6 @@
+public class stringLength {
+    public static void main(String[] args) {
+        String alphabets="ABCDEFGHIJKLMNOPQSTUVWXYZ";
+        System.out.println("the length:" + alphabets.length());
+    }
+}
