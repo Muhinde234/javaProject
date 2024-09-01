935bb5a09919e1e49e2aca44a3de070269e9e6ae
Muhinde <igirimpuhwedosta@gmail.com>
2024-09-01 15:57:36 +0200

    journey learning java

---
+++ Mathpow.java
@@ -0,0 +1,6 @@
+public class Mathpow {
+  public static void main(String[] args) {
+      int number=2;
+      System.out.println(Math.pow(number, 3));
+  }  
+}
---
+++ Mathsqrt.java
@@ -0,0 +1,6 @@
+public class Mathsqrt {
+  public static void main(String[] args) {
+      int number=25;
+      System.out.println(Math.sqrt(number));
+  }  
+}
