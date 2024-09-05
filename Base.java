dc30c77aa0ab1da70488cf10f2f17596f4082452
Muhinde <igirimpuhwedosta@gmail.com>
2024-09-05 17:44:00 +0200

    demostration of objects

---
+++ Base.java
@@ -0,0 +1,16 @@
+
+
+
+
+    public class Base {
+
+    int number=2;
+
+    public static void main(String[] args) {
+        Base test= new Base();
+       
+        System.out.println( test.number);
+    }
+    
+
+}
